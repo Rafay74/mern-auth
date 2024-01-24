85eec3057c56400fd7801e46ba3e91979d5608da
unknown <Abdul Rafay>
2024-01-24 16:00:12 +0500

    added db connection and env file

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to mongoDB')
+}).catch((err) => {
+    console.log("Error connecting to mongoDB", err)
+}) 
 
 const app = express()
 
