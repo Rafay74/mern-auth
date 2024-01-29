3e45d27acf81717ef285b90f2218155def6c2275
unknown <Abdul Rafay>
2024-01-29 17:12:38 +0500

    Added api routes for signup and sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,42 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
-export const signup = async (req,res, next) => {
-    const { username , email , password} = req.body;
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
     let pass = password.toString()
-    const hashedPassword = await bcryptjs.hash(pass,10)
-    const newUser = new User({username, email, password: hashedPassword })
+    const hashedPassword = await bcryptjs.hash(pass, 10)
+    const newUser = new User({ username, email, password: hashedPassword })
     try {
         await newUser.save()
-        res.status(201).json({message : "User created successfully"})     
+        res.status(201).json({ message: "User created successfully" })
     } catch (error) {
-        next(errorHandler(300,"Something went wrong"))
+        next(errorHandler(300, "Something went wrong"))
     }
-   
+
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        //check if the email is correct or not
+        const validUser = await User.findOne({ email })
+        if (!validUser) return next(errorHandler(404, 'User not found'))
+
+        //check password
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Invalid Password'))
+
+
+        //if both are correct , then make a token 
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        const { password: hashedPassword , ...rest } = validUser._doc
+        //set the token in the cookie
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+
+
+    } catch (error) {
+        next(error)
+    }
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link , useNavigate } from 'react-router-dom'
 
-const Signin = () => {
+const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    //sending data back to database 
+    e.preventDefault();
+    try {
+      setLoading(true)
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json()
+      setLoading(false)
+      if (data.success === false) {
+        setError(true)
+        return;
+      }
+      navigate('/')
+    } catch (error) {
+      setLoading(false)
+      setLoading(true)
+    }
+  }
   return (
-    <div>Signin</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form action="" className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        {/* <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} /> */}
+        <input type="text" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+        <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Sign In</button>
+      </form>
+      <div className='flex gap-1 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to="/sign-up">
+          <span className='text-blue-500'>Register</span>
+        </Link>
+      </div>
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
+    </div>
   )
 }
 
-export default Signin
+export default SignIn
